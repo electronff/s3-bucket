2747caa5d47fe63b88d97264b7f14e63e4316ce6
electronff <39598406+electronff@users.noreply.github.com>
2021-10-14 21:56:16 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
 provider "aws" {
-  version = "~> 3.0"
-  region  = "us-east-1"
+  region = "us-east-1"
 }
